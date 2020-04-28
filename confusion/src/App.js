bacb2a81696fd2245a6d86c4414feba88ba7c37a
vishwasprabhu98 <vishwasprabhu8@gmail.com>
2020-04-28 23:04:28 +0530

    part 2: using props information

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,23 +1,58 @@
-
-
-// original App.js
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Menu from './components/menuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { DISHES } from './shared/dishes.js'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      dishes : DISHES
+    };
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes = {this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+// original App.js
+// import React from 'react';
+// import logo from './logo.svg';
+// import Menu from './components/menuComponent';
+// import { Navbar, NavbarBrand } from 'reactstrap';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar dark color="primary">
+//         <div className="container">
+//           <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+//         </div>
+//       </Navbar>
+//       <Menu />
+//     </div>
+//   );
+// }
+
+// export default App;
