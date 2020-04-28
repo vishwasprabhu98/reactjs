7cc45c6adf1d5c5b4856e3542bfd0c716821477c
vishwasprabhu98 <vishwasprabhu8@gmail.com>
2020-04-28 23:43:58 +0530

    improper commit

--- confusion/src/components/dishDetails.js
+++ confusion/src/components/dishDetails.js
@@ -1,1 +1,41 @@
+
+
 import React, { Component } from 'react';
+
+class dishDetails extends Component{
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			selectedDish : this.props.dish
+		};
+	}
+
+	onDishSelect(dish) {
+		this.setState({ selectedDish : dish })
+	};
+
+	renderDish(dish){
+		if(dish != null){
+			return(
+				<Card>
+					<CardImg top src={dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>
+				</Card>
+			);
+		}
+		else{
+			return(
+				<div></div>
+			);
+		}
+	};
+}
+
+export default dishDetails;
+
+
--- confusion/src/components/menuComponent.js
+++ confusion/src/components/menuComponent.js
@@ -1,47 +1,25 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { dishDetails } from './dishDetails.js'
 
 class Menu extends Component {
 
 	constructor (props) {
 		super(props);
+
 		this.state = {
 			selectedDish: null
 		};
 	}
 
-	onDishSelect(dish) {
-		this.setState({ selectedDish : dish })
-	};
-
-	renderDish(dish){
-		if(dish != null){
-			return(
-				<Card>
-					<CardImg top src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
-			);
-		}
-		else{
-			return(
-				<div></div>
-			);
-		}
-	};
 
 	render () {
 
 		const menu = this.props.dishes.map((dish) => {
 			return (
 			<div key={dish.id} className="col-12 col-md-5 mt-5">
-				<Card key={dish.id}
-				onClick={() => this.onDishSelect(dish)}>
+				<Card key={dish.id} onClick={}>
 				<CardImg width="100%" src={dish.image} alt={dish.name} />
 				<CardImgOverlay>
 					<CardTitle>{dish.name}</CardTitle>
@@ -56,12 +34,6 @@ class Menu extends Component {
 			<div className="container">
 				<div className="row">
 					{ menu }
-				</div>
-				
-				<div className="row">
-					<div className="col-12 col-md-5 mt-4">
-						{this.renderDish(this.state.selectedDish)}
-					</div>
 				</div>
 			</div>
 		);
